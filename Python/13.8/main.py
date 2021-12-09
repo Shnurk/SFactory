3704bc3a5a1987680696ea8865cb3b2f7fb64fa6
Shnurk <47347453+Shnurk@users.noreply.github.com>
2021-12-09 17:42:46 +0300

    13.8 homewrok

---
+++ Python/13.8/main.py
@@ -0,0 +1,13 @@
+amount =int(input("Введите количество билетов:"))
+print("Для детей меньше 18 - бесплатно\n Для лиц от 18 до 25 - 990\n Для людей старше - 1390")
+years=[]
+summ=0
+for i in range(amount):
+    years.append(int(input("Введите возраст посетителя:")))
+for i in range(0, len(years)):
+    if years[i] > 18:
+        if years[i] < 25:
+            summ+=990
+        else:
+            summ+=1390
+print("Вы купили ",amount," билетов. C вас ", summ)
